5fb2adede43fcbd37e14faba4449e22333fad15b
texsmv <texs.mv@gmail.com>
2016-10-27 10:44:36 -0500

    migrations fix

--- app/Tramite.php
+++ app/Tramite.php
@@ -7,4 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 class Tramite extends Model
 {
     //
+    protected $table='tramites';
+
+    protected $fillable=['prioridad','entregado','area_id'];
+
+    public function area()
+    {
+        return $this->belongsTo('App\Area','area_id','id');
+    }
+   
 }
