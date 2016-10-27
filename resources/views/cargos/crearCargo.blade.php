ca159618edfed1e5ee28ad46b1822790f03bc046
Kat17 <kathy.uc.17@gmail.com>
2016-10-27 11:20:20 -0500

    crearCargo

--- resources/views/cargos/crearCargo.blade.php
+++ resources/views/cargos/crearCargo.blade.php
@@ -12,16 +12,38 @@
 
 @section('content')
 
-
-<h1>CREAR CARGO</h1>
 <form method="POST" action="crearCar">
 	{{ csrf_field()}}
-	Nombre Nuevo Cargo :
-	<input type="text" name="nombreCargo">
-	<br>
-	Descripción :
-	<input type="text" name="descripcion">
-	 <br> <input type="submit" value="Submit">
+
+	<div class="row">
+  		<div class="col-md-6 col-md-offset-3">
+
+			<h2><p class="text-center">  Crear Nuevo Cargo </p></h2>
+				<br><br>
+
+			<div class="row">
+				<div class="col-sm-12">
+					<label for="nomcargo" >Nombre de cargo: </label>
+					<div class="input-group">
+							<span class="input-group-addon" id="basic-addon1">
+								<span class="glyphicon glyphicon-briefcase" aria-hidden="true"></span>
+							</span>
+				  		<input class="form-control" type="text" name ="nombreCargo" id="nomcargo" placeholder="Ingrese nombre de cargo">
+					</div>
+				</div>
+			</div><br>
+			<div class="row">
+			  <div class="col-sm-12">
+			  		<label for="descrip" >Descripción: </label>
+				    <textarea class="form-control" placeholder="Ingrese la descripción del área"  name="descripcion" id="descripcion"></textarea>
+			  </div>
+			</div><br>
+
+			<button class="btn btn-lg btn-primary btn-block" type="submit" value="Submit">Crear nuevo cargo</button> 
+
+		</div>
+	</div>
+
 </form>
 
 @endsection
--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -86,7 +86,8 @@
            <li class="dropdown">
                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Cargos <span class="caret"></span></a>
                <ul class="dropdown-menu">
-                 <li><a href="{{ url('#')}}">Crear</a></li>
+                 <li><a href="{{ url('cargos/crear')}}">Crear</a></li>
+                 <li><a href="{{ url('#')}}">Editar</a></li>
                  <li><a href="{{ url('cargos/mostrar') }}">Mostrar todos</a></li>
                </ul>
            </li>
