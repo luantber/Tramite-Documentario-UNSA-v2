c8ce1b908d2c08c9790dbbd48abe4ce8c29a6a88
margarcuae <margar.tcuae@gmail.com>
2016-10-30 22:16:03 -0500

    wep

--- app/Http/Controllers/empleadosController.php
+++ app/Http/Controllers/empleadosController.php
@@ -15,9 +15,9 @@ class empleadosController extends Controller
     public function antesCrear(){
       $cargos = DB::table('cargos')->select('id','nombreCargo')->get();
       //dd($cargos[0]->id);
-      $fun = response()->json($cargos);
-      //dd($fun);
-      return view('empleados.crear',['$cargo'=>$fun]);
+      //$fun = response()->json($cargos);
+      //dd($fun[0].id);
+      return view('empleados.crear',['$cargo'=>$cargos]);
 
     }
 
