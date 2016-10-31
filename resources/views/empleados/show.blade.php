21d9226fea8b6c170e3bf33751c05be6e32b4dd9
Kat17 <kathy.uc.17@gmail.com>
2016-10-31 15:59:21 -0500

    perfi-emple

--- resources/views/empleados/show.blade.php
+++ resources/views/empleados/show.blade.php
@@ -3,13 +3,14 @@
 @section('title','Perfil Empleado')
 
 @section('content')
+
+{{ $empleado->user->id }}
 	
 	<div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xs-offset-0 col-sm-offset-0 col-md-offset-3 col-lg-offset-3 toppad" >
-  <input class="form-control" type="string"  value="{{$empleado->user->nombre}}" disabled="" required="true">
-   <!--
+   
           <div class="panel panel-info">
             <div class="panel-heading">
-              <h3 class="panel-title">Perfil de:  {{ $empleado->nombre }}</h3>
+              <h3 class="panel-title">Perfil de:  {{ $empleado->user->nombre }}</h3>
             </div>
             <div class="panel-body">
               <div class="row">
@@ -20,31 +21,31 @@
                     <tbody>
                       <tr>
                         <td>Nombre: </td>
-                        <td>{{ $empleado->nombre }}</td>
+                        <td>{{ $empleado->user->nombre }}</td>
                       </tr>
                       <tr>
                         <td>Apellidos: </td>
-                        <td>{{ $empleado->apellido}}</td>
+                        <td>{{ $empleado->user->apellido}}</td>
                       </tr>
                       <tr>
                         <td>DNI:</td>
-                        <td>{{ $empleado-> dni }}</td>
+                        <td>{{ $empleado->user->dni }}</td>
                       </tr>
                       <tr>
                         <td>Cargo:</td>
-                        <td>{{ $empleado-> cargo }}</td>
+                        <td>{{ $empleado-> cargo->nombreCargo }}</td>
                       </tr>
                       <tr>
                         <td>Area:</td>
-                        <td>{{ $empleado-> area }}</td>
+                        <td>{{ $empleado-> area -> nombre }}</td>
                       </tr>
                       <tr>
                         <td> e-mail: </td>
-                        <td>{{$empleado -> email}}</td>
+                        <td>{{$empleado -> user-> email}}</td>
                       </tr>
                       <tr>
                         <td> Activo: </td>
-                        <td>{{$empleado -> activo}}</td>
+                        <td>{{$empleado -> user-> activo}}</td>
                       </tr>
                      
                     </tbody>
@@ -54,13 +55,13 @@
               </div>
             </div>
                  <div class="panel-footer">
-                        <a href="{{asset('empleados')}}{{'/'.$empleado->id.'/editar'}}" data-original-title="Edit this user" data-toggle="tooltip" type="button" class="btn btn-sm btn-warning"><i class="glyphicon glyphicon-edit"></i></a>
+                        <a href="{{asset('empleados')}}{{'/'.$empleado->user->id.'/editar'}}" data-original-title="Edit this user" data-toggle="tooltip" type="button" class="btn btn-sm btn-warning"><i class="glyphicon glyphicon-edit"></i></a>
                         <span class="pull-right">
                             <a data-original-title="Remove this user" data-toggle="tooltip" type="button" class="btn btn-sm btn-danger"><i class="glyphicon glyphicon-remove"></i></a>
                         </span>
                     </div>
             
-          </div>-->
+          </div>
         </div>
 
 @endsection
