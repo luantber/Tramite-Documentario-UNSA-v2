22d9e5161760ddedce0289683681cc02df86903d
yaraquispe <yaira3228@gmail.com>
2016-11-03 17:13:54 -0500

    jano ahora si

--- app/notas.php
+++ app/notas.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class notas extends Model
 {
+    public function nota_empleado()
+    {
+    	return $this->belongsTo('App\Empleado','empleados_id','id');
+    }
+
+    public function nota_area()
+    {
+    	return $this->belongsTo('App\Area','areas_id','id');
+    }
+    
     //
 }
