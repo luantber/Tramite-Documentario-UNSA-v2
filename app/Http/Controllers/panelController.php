b4043e674aafc5b3f44bf14e1b8e6c4fef14b240
luisbch9 <luisbch9@gmail.com>
2016-11-06 19:36:42 -0500

    Panel Show

--- app/Http/Controllers/panelController.php
+++ app/Http/Controllers/panelController.php
@@ -21,7 +21,7 @@ class panelController extends Controller
     // dd(Auth::user()->empleado->id_area) ; 1
 
 	//dd(Tramite::all());
-		$tramites = Tramite::with('area','persona','empleado')->where('area_id',Auth::user()->empleado->id_area)->paginate(1);
+		$tramites = Tramite::with('area','persona','empleado')->where('area_id',Auth::user()->empleado->id_area)->join('estado_tramites', 'tramites.id', '=', 'estado_tramites.id')->where('show',1)->paginate(1);
         //dd($tramites);
         return response()->json($tramites);
     }
