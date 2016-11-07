b8a34cbc53db1baee5711284e158b250c0ad4b46
lamlemonpie <alejandrolarraondo@gmail.com>
2016-11-06 20:44:57 -0500

    Agenda - Eliminar/Show

--- resources/views/notas/show.blade.php
+++ resources/views/notas/show.blade.php
@@ -5,12 +5,12 @@
 @section('content')
 
   <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xs-offset-0 col-sm-offset-0 col-md-offset-3 col-lg-offset-3 toppad" >
-   
+
           <div class="panel panel-info">
             <div class="panel-body">
               <div class="row">
-               
-                <div class=" col-md-9 col-lg-9 "> 
+
+                <div class=" col-md-9 col-lg-9 ">
                   <table class="table table-user-information">
                     <tbody>
                       <tr>
@@ -28,9 +28,9 @@
               </div>
             </div>
                  <div class="panel-footer">
-                        <a href="{{asset('notas/todos')}}{{'/'.$nota->id.'/editar'}}" data-original-title="Edit this user" data-toggle="tooltip" type="button" class="btn btn-sm btn-warning"><i class="glyphicon glyphicon-edit"></i></a>
+                        <a href="{{asset('notas/todos')}}{{'/'.$notas->id.'/editar'}}" data-original-title="Edit this user" data-toggle="tooltip" type="button" class="btn btn-sm btn-warning"><i class="glyphicon glyphicon-edit"></i></a>
                         <span class="pull-right">
-                            <a href="{{asset('notas')}}{{'/'.$eliminado->id}}" data-original-title="Remove this user" data-toggle="tooltip" type="button" class="btn btn-sm btn-danger"><i class="glyphicon glyphicon-remove"></i></a>
+                            <a href="{{asset('notas')}}{{'/'.$notas->id.'/eliminar'}}" data-original-title="Remove this user" data-toggle="tooltip" type="button" class="btn btn-sm btn-danger"><i class="glyphicon glyphicon-remove"></i></a>
                         </span>
                     </div>
 
