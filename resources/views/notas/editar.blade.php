6e9791d46aa16a1ef378346489d7d0f41c0bff93
yaraquispe <yaira3228@gmail.com>
2016-11-06 21:30:16 -0500

    editar

--- resources/views/notas/editar.blade.php
+++ resources/views/notas/editar.blade.php
@@ -4,41 +4,60 @@
 
 @section('content')
 
-<h2><p class="text-center">  Editar Área </p></h2>
+<div class="row">
+      <div class="col-md-6 col-md-offset-3">
+
+    <h2><p class="text-center">  Editar Notas </p></h2>
 
     <br><br>
-    <form method="POST" action="{{ asset('notas')}}{{'/'.$notas->id}}">
-    {{ csrf_field()}}
+      <form method="post" onsubmit="return validar()" action="{{asset('notas')}}{{'/'.$nota->id}}">
 
-          <div class="row">
+        {{ csrf_field()}}
 
-            <div class="col-sm-12">
-          <label for="nombre"> Nombre de la Nota: </label>
-          <div class="input-group">
-              <span class="input-group-addon" id="basic-addon1">
-                <span class="glyphicon glyphicon-briefcase" aria-hidden="true"></span>
-              </span>
-              <input type="text" class="form-control"name="nombre" id="nombre" value="{{$notas->nombre}}">
+        <div class="row">
+          <div class="col-sm-12">
+            <label for="nombre" >Nombre: </label>
+            <div class="input-group">
+                <span class="input-group-addon" id="basic-addon1">
+                  <span class="glyphicon glyphicon-tag" aria-hidden="true"></span>
+                </span>
+              <input class="form-control" type="string" name ="nombre" id="nombre" value="{{$nota->nombre}}"  required="true">
             </div>
           </div>
-        </div>
-      <div class="form-group">
-          <label for="descripcion"> Descripción: </label>
-            <input class="form-control" value="{{ $nota->descripcion }}"  name="descripcion" id="descripcion" required="">
+        </div><br>
+
+        <div class="row">
+          <div class="col-sm-12">
+              <label for="apellido" >Nota: </label>
+            <div class="input-group">
+                <span class="input-group-addon" id="basic-addon1">
+                  <span class="glyphicon glyphicon-pencil" aria-hidden="true"></span>
+                </span>
+                <input class="form-control" type="string" name ="apellido" id="apellido" value="{{$nota->descripcion}}"  required="true">
+              </div>
+          </div>
+        </div><br>
+
+        <div class="row">
+          <div class="col-sm-6">
+            <label for="area">Privacidad: </label>
+                 <select name="personal" type="text" class="form-control" id="personal" name="personal" required="true">
+                <option value="" >Cambiar Privacidad</option>
+                          <option value='1'>Personal</option>
+                          <option value='0'>Público</option>
+                </select>
+          </div>
+
+        </div><br>
+        <div class="form-group">
+            <div class="text-center">
+            <button class="btn btn-lg" type="submit" value="Submit"> 
+            Guardar cambios </button> 
+          </div>
         </div>
 
-        <div class="text-center">
-            <button type="submit" value="Submit" class="btn btn-primary">Guardar</button>
-        </div>
-
-        <ul class="pager">
-            <li><a href="{{asset('notas/todos')}}{{'/'.$notas->id.'/editar'}}">Cancelar</a></li>
-        </ul>
-
+      </form>
     </div>
-
-    </form>
-
-</form>
+  </div>
 
 @endsection
