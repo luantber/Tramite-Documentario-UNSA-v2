d47494815c68399939ee256e4ce739672d20317f
yaraquispe <yaira3228@gmail.com>
2016-11-09 11:46:34 -0500

    view eliminar

--- resources/views/notas/eliminar.blade.php
+++ resources/views/notas/eliminar.blade.php
@@ -9,20 +9,20 @@
   {{ csrf_field()}}
   <input type="hidden" name="id" value="{{$eliminado->id}}">
 
-  <div class="col-xs-6 col-sm-6 col-md-4 col-lg-4 col-xs-offset-0 col-sm-offset-0 col-md-offset-2 col-lg-offset-2 toppad" >
+<div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xs-offset-0 col-sm-offset-0 col-md-offset-3 col-lg-offset-3 toppad" >
 
-        <div class="panel panel-info">
-          <div class="panel-heading">
-            <div class="text-center">
-              <h3 class="panel-title">Nota</h3>
-            </div>
+      <div class="panel panel-info">
+        <div class="panel-heading">
+          <div class="text-center">
+            <h3 class="panel-title">Nota: Eliminar</h3>
           </div>
-          <div class="panel-body">
-            <div class="row">
-
-              <div class=" col-md-6 col-lg-6 ">
-                <table class="table table-user-information">
-                  <tbody>
+        </div>
+        <div class="panel-body">
+          <div class="row">
+           
+            <div class=" col-md-12 col-lg-12 "> 
+              <table class="table table-user-information">
+                <tbody>
                     <tr>
                       <td>Nombre : </td>
                       <td>{{$eliminado->nombre}}</td>
@@ -41,23 +41,21 @@
 
                     </tr>                    
                   </tbody>
-                </table>
+              </table>
 
-              </div>
             </div>
           </div>
         </div>
+      </div>
+
+
       <div class="text-center">
         <button type="submit" class="btn btn-info btn-lg" value="" data-toggle="modal" data-target="">Eliminar</button>
       </div>
 
-
-
-
-        <ul class="pager">
+      <ul class="pager">
             <li><a href="{{asset('notas')}}{{'/'.$eliminado->id}}">Cancelar</a></li>
-        </ul>
-
+      </ul>
 
     </div>
 
@@ -67,3 +65,6 @@
 
 
 @endsection
+
+
+
