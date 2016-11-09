2f913e06ce184fb0766abe528fa43663232fac83
texsmv <texs.mv@gmail.com>
2016-11-09 10:44:34 -0500

    fix

--- app/Http/Controllers/tiposDocumentoController.php
+++ app/Http/Controllers/tiposDocumentoController.php
@@ -3,55 +3,72 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\TipoTramite;
-use App\Tramite;
+
 use App\Http\Requests;
+use App\TipoTramite;
 
-class tiposDocumentoController extends Controller
+class tiposTramiteController extends Controller
 {
-    public function create(Request $datos){
-    	
-    	$nuevo = new TipoDocumento;
-    	$nuevo->nombre = $datos->nomTipo;    	
-    	$nuevo->descripcion = $datos->descripcion;
 
-    	$nuevo->save();
-      
-      return redirect('tiposDocumento');  
 
-    public function crearGet(){
+    public function todos()
+    {
+        $TipoDocumentos = TipoDocumento::all();
+        return response()->json($tiposDocumento);
+    }
+	public function crearGet(){
 
-		
 		return view('tiposDocumento.crear');
 	}
-	/*
-	public function eliminar($id){
-      $encontrado = Empleado::find($id);
-      if ($encontrado == null) {
-           return view ('creartramite');
-        }
-        return view('tipoDocumento.eliminar',['eliminado'=>$encontrado]);
+
+    public function crear(Request $datos){
+
+    	
+    	$nuevo = new TipoDocumento;
+    	$nuevo->nombre = $datos->nomDoc;
+    	$nuevo->descripcion = $datos->descripcion;
+    	$nuevo->save();
+
+    	return redirect('tiposDocumento');
     }
-	*/
-    public function eliminado(Request $datos){
-      $eliminado = TipoTramite::find($datos->id);
-      $eliminado->delete();
-      return redirect('tiposDocumento');
+
+    public function show($id){
+        $tipoDocumento = TipoDocumento::find($id);
+        return view('tiposDocumento.show',['tipoDocumento'=>$tipoDocumento]);
     }
-	
-	public function editar($id)
+
+    public function editar($id)
     {
         $tipoDocumento = TipoDocumento::find($id);
+
         return view('tiposDocumento.editar',['tipoDocumento'=>$tipoDocumento]);
     }
 
     public function guardar($id,Request $datos)
     {
-        $tipoDocumento = TipoDocumento::find($id);
+        $tipoTramite = TipoTramite::find($id);
+        $tipoTramite->nombre = $datos->nombre;
+    	$tipoTramite->descripcion = $datos->descripcion;
 
-        $tipoDocumento->nombre=$datos->nomTipo;
-        $tipoDocumento->descripcion=$datos->descripcion;
-        $tipoDocumento->save();
-        return redirect('tiposDocumento');
+        $tipoTramite->save();
+
+        return redirect('tipostramite');
+    }
+
+      public function eliminarGet($id){
+        $encontrado = TipoTramite::find($id);
+        if ($encontrado == null) {
+            echo "error NO EXISTE EL Tramite";
+        }
+      return view('tiposTramite.eliminar',['eliminado'=>$encontrado]);
+        
+    }
+
+
+    public function eliminar(Request $datos){
+        //dd("hola");
+        $eliminado = TipoTramite::find($datos->id);
+        $eliminado->delete();
+        return redirect('tipostramite');
     }
 }
